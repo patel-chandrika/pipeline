8ed74eafcbe7215a7e5afc6103be8cbfb1a81036
patel-chandrika <patelchandrika5062@gmail.com>
2024-08-15 17:41:58 +0530

    Initial commit

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student 
+{
+ public static void main(String[] args)
+  {
+     int a=5;
+     int b=6;
+     int c=a*b;
+     System.out.println("multiplication of  " +  a + " and  " + b + " : " + c);
+
+ }
+    
+}
